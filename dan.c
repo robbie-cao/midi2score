55432c75f64eb8a681780bcf1748145a932f65c9
Robbie Cao <robbie.cao@gmail.com>
2016-11-21 18:51:19 +0800

    Format code
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- dan.c
+++ dan.c
@@ -30,7 +30,7 @@ static struct {
         .suffix = ".drums",
     },
 
-    //leave this last. Using it to stop iteration.
+    // Leave this last. Using it to stop iteration.
     { .part = NULL }
 };
 
@@ -43,10 +43,11 @@ static inline int output_time(midi_t * const, char * file);
 static inline char * part_filename(char const * str, const char * file, const char * suffix);
 static inline midi_track_t * find_track(midi_t * midi, const char * part);
 
-int main(int argc, char**argv) {
+int main(int argc, char**argv)
+{
 
     char * midi_file;
-    if ( argc != 2 || strlen(argv[1]) < 1) {
+    if (argc != 2 || strlen(argv[1]) < 1) {
         char * invocation = (argc > 0) ? argv[0] : "./midi";
         fprintf(stderr, "Usage: %s filename.mid\n\n", invocation);
         return 1;
@@ -58,7 +59,8 @@ int main(int argc, char**argv) {
 
 }
 
-static inline int do_midi_thing(char * midi_file) {
+static inline int do_midi_thing(char * midi_file)
+{
     midi_t *midi;
     int status;
     int retn = 0;
@@ -70,29 +72,29 @@ static inline int do_midi_thing(char * midi_file) {
         return 1;
     }
 
-    //find the maximum length of all the filename suffixes.
+    // Find the maximum length of all the filename suffixes.
     unsigned int max_suffix_len = 0;
 
-    for ( int i = 0; track_map[i].part != NULL; ++i )
-        if ( strlen(track_map[i].suffix) > max_suffix_len )
+    for (int i = 0; track_map[i].part != NULL; ++i)
+        if (strlen(track_map[i].suffix) > max_suffix_len)
             max_suffix_len = strlen(track_map[i].suffix);
 
 
-    char * partfile = malloc(strlen(midi_file)+max_suffix_len+1);
+    char * partfile = malloc(strlen(midi_file) + max_suffix_len + 1);
 
 
-    for ( int i = 0; track_map[i].part != NULL; ++i ) {
+    for (int i = 0; track_map[i].part != NULL; ++i) {
         midi_track_t * trk = find_track(midi, track_map[i].part);
 
-        if ( trk == NULL ) {
+        if (trk == NULL) {
             fprintf(stderr, "Failed to find %s\n", track_map[i].part);
             retn = 1;
             continue;
         }
 
-        partfile = part_filename(partfile,midi_file,track_map[i].suffix);
+        partfile = part_filename(partfile, midi_file, track_map[i].suffix);
 
-        if ( output_track(trk, partfile) ) {
+        if (output_track(trk, partfile)) {
             fprintf(stderr, "Failed to output %s to %s\n", track_map[i].part, partfile);
             retn = 1;
         }
@@ -100,48 +102,50 @@ static inline int do_midi_thing(char * midi_file) {
         midi_free_track(trk);
     }
 
-    if ( output_time(midi,part_filename(partfile,midi_file,TIME_SUFFIX)) ) {
+    if (output_time(midi, part_filename(partfile, midi_file, TIME_SUFFIX))) {
         fprintf(stderr, "failed to output time\n");
         retn = 1;
     }
 
-    //end:
+    // End
     midi_close(midi);
 
     return retn;
 
 }
 
-static inline char * part_filename(char const * str, const char * file, const char * suffix) {
-    strncpy((char *)str, (char *)file, strlen(file)+1);
-    strncat((char *)str, (char *)suffix, strlen(suffix)+1);
+static inline char * part_filename(char const * str, const char * file, const char * suffix)
+{
+    strncpy((char *)str, (char *)file, strlen(file) + 1);
+    strncat((char *)str, (char *)suffix, strlen(suffix) + 1);
     return (char*)str;
 }
 
-static inline int output_track(midi_track_t * const trk, char* fname) {
+static inline int output_track(midi_track_t * const trk, char* fname)
+{
 
     // printf("Track %d, %d events, %u bytes, sig: %c%c%c%c\n", trk->num, trk->events, trk->hdr.size,
     //      trk->hdr.magic[0], trk->hdr.magic[1], trk->hdr.magic[2], trk->hdr.magic[3]);
 
     FILE * file = fopen(fname, "w+");
 
-    if ( file == NULL )
+    if (file == NULL) {
         return 1;
+    }
 
     trk->cur = trk->head;
-
 
     unsigned long int absolute = 0;
 
     midi_iter_track(trk);
     midi_event_t * cur;
-    while ( midi_track_has_next(trk) ) {
+    while (midi_track_has_next(trk)) {
         cur = midi_track_next(trk);
-        if ( cur->type == MIDI_EVENT_TYPE_EVENT) {
+        if (cur->type == MIDI_EVENT_TYPE_EVENT) {
             absolute += cur->delta_time;
             midi_event_t * event = cur;
 
-            if ( event->cmd == MIDI_EVENT_NOTE_ON || event->cmd == MIDI_EVENT_NOTE_OFF )
+            if (event->cmd == MIDI_EVENT_NOTE_ON || event->cmd == MIDI_EVENT_NOTE_OFF)
                 fprintf(file, "%lu,%u,%u\n", absolute, event->data[0],event->data[1]);
 
         } else if (cur->type == MIDI_EVENT_TYPE_META) {
@@ -154,28 +158,30 @@ static inline int output_track(midi_track_t * const trk, char* fname) {
     return 0;
 }
 
-static inline int output_time(midi_t * const midi, char * fname) {
+static inline int output_time(midi_t * const midi, char * fname)
+{
 
     midi_track_t * trk =  midi_get_track(midi, 0);
 
-    if ( trk == NULL ) {
+    if (trk == NULL) {
         fprintf(stderr, "Failed to find track 0... WTF?\n");
         return 1;
     }
 
     FILE * file = fopen(fname, "w+");
 
-    if ( file == NULL )
+    if (file == NULL) {
         return 1;
+    }
 
 
     midi_iter_track(trk);
     unsigned long int absolute = 0;
-    while ( midi_track_has_next(trk) ) {
+    while (midi_track_has_next(trk)) {
         midi_event_t * cur = midi_track_next(trk);
         absolute += cur->delta_time;
 
-        if ( cur->type == MIDI_EVENT_TYPE_META && cur->cmd == 0x58 )
+        if (cur->type == MIDI_EVENT_TYPE_META && cur->cmd == 0x58)
             fprintf(file, "%lu,%u,%u\n", absolute,cur->data[0], cur->data[1]);
     }
 
@@ -185,13 +191,14 @@ static inline int output_time(midi_t * const midi, char * fname) {
 }
 
 
-//this is a pretty inefficient way of doing this, but I don't really feel like making
-//the midi code "public" and using it mroe... besides this is easier anyway!
+// This is a pretty inefficient way of doing this, but I don't really feel like making
+// the midi code "public" and using it mroe... besides this is easier anyway!
 
-static inline midi_track_t * find_track(midi_t * midi, const char * part) {
+static inline midi_track_t * find_track(midi_t * midi, const char * part)
+{
     midi_track_t * track;
 
-    for ( int i = 0; i < midi->hdr.tracks; ++i ) {
+    for (int i = 0; i < midi->hdr.tracks; ++i) {
         track =  midi_get_track(midi, i);
 
 
@@ -200,22 +207,22 @@ static inline midi_track_t * find_track(midi_t * midi, const char * part) {
 
         midi_iter_track(track);
         midi_event_t * evnt;
-        while ( midi_track_has_next(track) ) {
+        while (midi_track_has_next(track)) {
             evnt = midi_track_next(track);
 
-            if ( evnt->delta_time != 0 )
+            if (evnt->delta_time != 0)
                 break;
             //0x03 = track name :D
-            if ( evnt->type != MIDI_EVENT_TYPE_META || evnt->cmd != 0x03 )
+            if (evnt->type != MIDI_EVENT_TYPE_META || evnt->cmd != 0x03)
                 continue;
 
-            if ( evnt->size == strlen(part)
-                    &&  !strncmp((const char *)evnt->data, part, evnt->size) ) {
+            if (evnt->size == strlen(part)
+                    &&  !strncmp((const char *)evnt->data, part, evnt->size)) {
                 return track;
             }
         }
 
-        if ( track != NULL )
+        if (track != NULL)
             midi_free_track(track);
     }
 
